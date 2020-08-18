347440d6adb22f04bacab0f0c8792ec32a380bf5
NeutralNoise <classicbrad24@gmail.com>
2020-08-19 02:41:50 +1000

    Added simple memcpy()

--- src/tier0/string.h
+++ src/tier0/string.h
@@ -8,4 +8,11 @@ size_t strlen(const char *s) {
     return s-a;
 }
 
+void * memcpy(void * dest, void * src, size_t num) {
+    uint8_t * d = (uint8_t*)dest;
+    const uint8_t * s = (uint8_t*)src;
+    for(; num; num--) *d++ = *s++;
+    return (void*)dest;
+}
+
 #endif //STRING_H_INCLUDED
