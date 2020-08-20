8a3503f6ef5f48c176acfe293cfcc98d48471a5c
NeutralNoise <classicbrad24@gmail.com>
2020-08-21 03:57:47 +1000

    Added uitoa function.

--- src/tier0/stdlib.h
+++ src/tier0/stdlib.h
@@ -67,4 +67,26 @@ char* itoa(int value, char* result, int base) {
     return result;
 }
 
+char* uitoa(unsigned int value, char* result, int base) {
+    // check that the base if valid
+    if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+    char* ptr = result, *ptr1 = result, tmp_char;
+    unsigned int tmp_value;
+
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+    } while ( value );
+    //Add the null terminator.
+    *ptr-- = '\0';
+    while(ptr1 < ptr) {
+      tmp_char = *ptr;
+      *ptr--= *ptr1;
+      *ptr1++ = tmp_char;
+    }
+    return result;
+}
+
 #endif
