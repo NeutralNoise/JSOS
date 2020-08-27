decf4452df1bef6842d19e20825d9a562579ef8a
NeutralNoise <classicbrad24@gmail.com>
2020-08-27 20:30:56 +1000

    Added strcmp() and strcat().

--- src/tier0/string.h
+++ src/tier0/string.h
@@ -2,17 +2,34 @@
 #define STRING_H_INCLUDED
 #include "types.h"
 
+void * memcpy(void * dest, void * src, size_t num) {
+    uint8_t * d = (uint8_t*)dest;
+    const uint8_t * s = (uint8_t*)src;
+    for(; num; num--) *d++ = *s++;
+    return (void*)dest;
+}
+
+void * memset(void * dest, int c, size_t n) {
+    unsigned char *s = (unsigned char *)dest;
+    for (; n; n--, s++) *s = c;
+    return dest;
+}
+
 size_t strlen(const char *s) {
     const char *a = s;
     for(; *s;s++);
     return s-a;
 }
 
-void * memcpy(void * dest, void * src, size_t num) {
-    uint8_t * d = (uint8_t*)dest;
-    const uint8_t * s = (uint8_t*)src;
-    for(; num; num--) *d++ = *s++;
-    return (void*)dest;
+char * strcat(char * dest, const char * src) {
+    char * d = dest + strlen(dest);
+    for(; (*d = *src); src++, d++);
+    return d;
+}
+
+int strcmp(const char*l, const char *r) {
+    for(; *l == *r && *l; l++, r++);
+    return *(unsigned char*)l - *(unsigned char*)r;
 }
 
 #endif //STRING_H_INCLUDED
