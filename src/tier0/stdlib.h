8ed24f3057e06ad558b13678a61051e09b0347a3
NeutralNoise <classicbrad24@gmail.com>
2021-03-23 21:47:08 +1100

    Added operator new for c++. so code clean up.

--- src/tier0/stdlib.h
+++ src/tier0/stdlib.h
@@ -1,7 +1,5 @@
 #ifndef STD_LIB_H_INCLUDED
 #define STD_LIB_H_INCLUDED
-//This is just here when working on this file. Its a bitch to read other wise.
-//#define __JS_OS
 
 //This needs to be defined with the -D compile flag.
 #ifdef __JS_OS
@@ -18,22 +16,34 @@
 #endif
 
 void* malloc(const unsigned int &size) {
-  #ifdef __JS_OS
+  #if defined(__JS_OS)
   if(MemoryManager::p_activeMemoryManager) {
     return MemoryManager::p_activeMemoryManager->Malloc(size);
   }
   else {
     return nullptr;
   }
+  #elif defined(__linux__) && !defined(__JS_OS)
+  #error "No malloc for linux build!"
+  #elif defined(__WIN32) && !defined(__JS_OS)
+  #error "No malloc for windows build!"
+  #else
+  #error "unknown OS so there is no way there is a malloc()."
   #endif //__JS_OS this is what we do on JSOS
   //TODO Work out windows and linux codes.
 }
 
 void free(void* ptr) {
-  #ifdef __JS_OS
+  #if defined(__JS_OS)
   if(MemoryManager::p_activeMemoryManager) {
     MemoryManager::p_activeMemoryManager->Free(ptr);
   }
+  #elif defined(__linux__) && !defined(__JS_OS)
+  #error "No free() for linux build!"
+  #elif defined(__WIN32) && !defined(__JS_OS)
+  #error "No free() for windows build!"
+  #else
+  #error "unknown OS so there is no way there is a free()."
   #endif //__JS_OS this is what we do on JSOS
   //TODO Work out windows and linux codes.
 }
@@ -103,5 +113,30 @@ int atoi(const char *s) {
 		n = 10*n - (*s++ - '0');
 	return neg ? n : -n;
 }
+#ifdef __cplusplus
+void* operator new(size_t size) {
+  return malloc(size);
+}
 
+void* operator new[](size_t size) {
+  return malloc(size);
+}
+
+//Placement new.
+void* operator new(size_t size, void *ptr) {
+  return ptr;
+}
+
+void* operator new[](size_t size, void *ptr) {
+  return ptr;
+}
+
+void operator delete(void* ptr) {
+  free(ptr);
+}
+
+void operator delete[](void* ptr) {
+  free(ptr);
+}
+#endif //__cplusplus
 #endif
