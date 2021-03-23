89ad8f42ef68c84456b91b6755b7e88d06e6d7a8
NeutralNoise <classicbrad24@gmail.com>
2021-03-23 21:44:57 +1100

    Added NULL defines.

--- src/tier0/defines.h
+++ src/tier0/defines.h
@@ -3,4 +3,13 @@
 
 #define MB 1000000 //!< 1 MB as bytes.
 
+#ifdef NULL
+#undef NULL
+//#define NULL 0
+#define NULL nullptr;
+#else
+//#define NULL 0
+#define NULL nullptr;
+#endif
+
 #endif //DEFINES_H_STD_INCLUDED
