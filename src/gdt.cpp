938a56525dc10f70c9d855d63e1af5cd512f1711
NeutralNoise <classicbrad24@gmail.com>
2021-08-28 21:18:55 +1000

    Fixed wrong type set for codeSegmentSelector.

--- src/gdt.cpp
+++ src/gdt.cpp
@@ -1,6 +1,7 @@
 #include "gdt.h"
 
-GlobalDescriptorTable::GlobalDescriptorTable() : nullSegmentSelector(0,0,0), unusedSegmentSelector(0,0,0), codeSegmentSelector(0,64*1024*1024, 0x94),
+GlobalDescriptorTable::GlobalDescriptorTable() : nullSegmentSelector(0,0,0), unusedSegmentSelector(0,0,0),
+codeSegmentSelector(0,64*1024*1024, 0x9A),
 dataSegmentSelector(0,64*1024*1024, 0x92) {
 
   uint32_t i[2];
